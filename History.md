3af990026b565ddb4bb97252b11741a2de225183
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-18 21:26:06 -0700

    first commit

---
+++ History.md
@@ -0,0 +1,11 @@
+0.0.3 - September 18, 2013
+--------------------------
+* handle unknown cases
+
+0.0.2 - September 18, 2013
+--------------------------
+* support acronyms at the end of a string
+
+0.0.1 - September 18, 2013
+--------------------------
+:sparkles:
---
+++ test/tests.js
@@ -0,0 +1,30 @@
+describe('to-no-case', function () {
+
+var assert = require('assert');
+var none = require('to-no-case');
+
+it('should remove slug case', function () {
+  assert('a slug case string' == none('a-slug-case-string'));
+});
+
+it('should remove snake case', function () {
+  assert('a snake case string' == none('a_snake_case_string'));
+});
+
+it('should remove camel case', function () {
+  assert('a camel case string' == none('aCamelCaseString'));
+});
+
+it('should remove constant case', function () {
+  assert('a constant case string' == none('A_CONSTANT_CASE_STRING'));
+});
+
+it('should remove sentence case', function () {
+  assert('a sentence case string.' == none('A sentence case string.'));
+});
+
+it('should remove title case', function () {
+  assert('a title: case of string' == none('A Title: Case of String'));
+});
+
+});
